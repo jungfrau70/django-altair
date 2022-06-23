faea3dece81979da600eb57bb5e55d0099fec218
InHwan Jung <inhwan.jung@gmail.com>
2022-06-23 14:09:19 -0700

    2022_06_23_02_09_PM

--- dashboard/app/views.py
+++ dashboard/app/views.py
@@ -12,24 +12,38 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = locals()
 
-
-        data3 = pd.DataFrame({
-            'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-            'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
-        })
-
-        context['chart'] = alt.Chart(data3).mark_bar().encode(
-            x='a',
-            y='b'
-        ).interactive()
-
         source = data.cars()
+
+        context['chart'] = alt.Chart(source).mark_circle().encode(
+            x='Horsepower',
+            y='Miles_per_Gallon',
+            color='Origin'
+        ).interactive()
 
         context['chart2'] = alt.Chart(source).mark_circle().encode(
             x='Horsepower',
             y='Miles_per_Gallon',
             color='Origin'
         ).interactive()
+        # return render(request, self.template_name, context)    
+
+        # data3 = pd.DataFrame({
+        #     'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+        #     'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+        # })
+
+        # context['chart'] = alt.Chart(data3).mark_bar().encode(
+        #     x='a',
+        #     y='b'
+        # ).interactive()
+
+        # source = data.cars()
+
+        # context['chart2'] = alt.Chart(source).mark_circle().encode(
+        #     x='Horsepower',
+        #     y='Miles_per_Gallon',
+        #     color='Origin'
+        # ).interactive()
 
 
 
